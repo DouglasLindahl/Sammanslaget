2d03851880ff5bb7c4bd14f7d9fd1f24b10cc21e
DouglasLindahl <douglas.lindahl@gmail.com>
2023-08-25 08:41:33 +0200

    new header position

--- script.js
+++ script.js
@@ -10,5 +10,30 @@ imageButtons.forEach(button => {
 });
 
 
-// 2, 12, 1112, 3112, 132112 
+
+// Get the header element
+const header = document.querySelector('header');
+
+// Calculate the initial top offset of the header
+const initialOffset = header.getBoundingClientRect().top + window.scrollY;
+
+// Function to handle scroll events
+function handleScroll() {
+    // Calculate the current scroll position
+    const currentScroll = window.scrollY;
+
+    // If the header is about to go off the top of the screen, make it fixed at the top
+    if (initialOffset - currentScroll <= 0) {
+        header.style.position = 'fixed';
+        header.style.top = '0';
+    } else {
+        // Otherwise, reset it to its original position
+        header.style.position = 'absolute';
+        header.style.top = initialOffset + 'px';
+    }
+}
+
+// Add a scroll event listener to the window
+window.addEventListener('scroll', handleScroll);
+
 
